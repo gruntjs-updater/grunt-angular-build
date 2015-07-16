927d5c47ae177191f61bbba2ac318d4c3759e4d6
Michelle Darlea <mdarlea@gmail.com>
2015-07-16 00:11:09 -0400

    Added the html2js configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,45 +13,59 @@ module.exports = function(grunt) {
   // Project configuration.
     grunt.initConfig({
       debug: {
-        options: {
-            open: false // do not open node-inspector in Chrome automatically 
+            options: {
+                open: false // do not open node-inspector in Chrome automatically 
+            }
+        },
+        html2js: {
+            dist: {
+                options: {
+                    module: null, // no bundle module for all the html2js templates
+                    base: '.'
+                },
+                files: [{
+                        expand: true,
+                        src: ['template/**/*.html'], ext: '.html.js'
+                    }]
+            }
+        },
+        jshint: {
+          all: [
+            'Gruntfile.js',
+            'tasks/*.js',
+            '<%= nodeunit.tests %>'
+          ],
+          options: {
+            jshintrc: '.jshintrc'
+          }
+        },
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+          tests: ['tmp']
+        },
+
+        // Configuration to be run (and then tested).
+        build: {
+            options: {
+                filename: 'sw-common',
+                prefix: 'sw',
+                modulePrefix: 'sw',
+                moduleName: 'sw.common'
+            }      
+        },
+
+        // Unit tests.
+        nodeunit: {
+          tests: ['test/*_test.js']
         }
-    },
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
-    // Configuration to be run (and then tested).
-    build: {
-        options: {
-            filename: 'sw-common',
-            prefix: 'sw',
-            modulePrefix: 'sw',
-            moduleName: 'sw.common'
-        }      
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
 
   });
+    
+ grunt.loadNpmTasks('grunt-html2js');
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+ // Actually load this plugin's task(s).
+ grunt.loadTasks('tasks');
     
   grunt.loadNpmTasks('grunt-debug-task');
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -61,12 +75,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    
+  grunt.registerTask('before-test', ['jshint', 'html2js', 'clean']);
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'build', 'nodeunit']);
+  grunt.registerTask('test', ['before-test', 'build', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'clean', 'build']);
+  grunt.registerTask('default', ['before-test', 'build']);
 
 };
